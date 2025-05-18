4e39f69447910b79f6b8a8b32da9a03afcae1822
unknown <skaliaksei@gmail.com>
2025-05-18 22:32:02 +0300

    create modal window

---
+++ scripts/modal.js
@@ -0,0 +1,24 @@
+function loadCSS(href) {
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = href;
+    document.head.appendChild(link);
+}
+
+function loadModalHTML(url, containerId) {
+  fetch(url)
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById(containerId).innerHTML = html;
+    });
+}
+
+document.getElementById("openModal").addEventListener("click", () => {
+  loadCSS("../reviews-page/css/modal-style.css");
+  loadModalHTML("./reviews-page/modal.html", "modalContainer");
+});
+
+document.getElementById("openModal-2").addEventListener("click", () => {
+  loadCSS("../reviews-page/css/modal-style.css");
+  loadModalHTML("./reviews-page/modal.html", "modalContainer");
+});
