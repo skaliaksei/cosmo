4a99802cc74f373bcb0f2bf98d5dd78fec11b0a0
unknown <skaliaksei@gmail.com>
2025-05-19 11:18:33 +0300

    close Modal window

--- scripts/modal.js
+++ scripts/modal.js
@@ -14,11 +14,29 @@ function loadModalHTML(url, containerId) {
 }
 
 document.getElementById("openModal").addEventListener("click", () => {
+  const modal = document.getElementById("modalContainer");
+  modal.style.display = "block";
+
   loadCSS("../cosmo/reviews-page/css/modal-style.css");
   loadModalHTML("./reviews-page/modal.html", "modalContainer");
 });
 
 document.getElementById("openModal-2").addEventListener("click", () => {
+  const modal = document.getElementById("modalContainer");
+  modal.style.display = "block";
+
   loadCSS("../cosmo/reviews-page/css/modal-style.css");
   loadModalHTML("./reviews-page/modal.html", "modalContainer");
+});
+
+// Close Modal
+document.addEventListener("click", (e) => {
+    const modal = document.getElementById("modalContainer");
+    
+    if (e.target.classList.contains("modal-close") || e.target.classList.contains("modal-overlay")) {
+      modal.innerHTML = ""; // очищаем модалку
+      modal.style.display = "none"; // или любое другое изменение стиля
+      // modal.style.backgroundColor = "transparent"; // например
+      //modal.classList.remove("active"); // если был добавлен класс
+    }
 });
