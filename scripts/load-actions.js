a064a75e22abacfcb286b3e72d1b20e14fd7314f
unknown <skaliaksei@gmail.com>
2025-05-22 12:10:28 +0300

    actions to component

---
+++ scripts/load-actions.js
@@ -0,0 +1,15 @@
+async function loadComponent(htmlPath, cssPath, targetSelector) {
+  // 1. Загружаем HTML
+  const html = await fetch(htmlPath).then(res => res.text());
+
+  // 2. Добавляем CSS
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = cssPath;
+  document.head.appendChild(link);
+
+  // 3. Вставляем в DOM
+  document.querySelector(targetSelector).innerHTML += html;
+}
+
+loadComponent("components/actions.html", "css/actions.css", "#actions");
